575647c032dcc449a973d81266e75ca8c2abe9d5
Weverton Avelino <wtonja@gmail.com>
2019-10-07 21:38:51 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/nelioalves/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/StandardError.java
@@ -3,11 +3,13 @@ package com.nelioalves.cursomc.resources.exception;
 import java.io.Serializable;
 
 public class StandardError implements Serializable{
-	
 	private static final long serialVersionUID = 1L;
+	
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
+	
+	
 	public StandardError(Integer status, String msg, Long timeStamp) {
 		super();
 		this.status = status;
