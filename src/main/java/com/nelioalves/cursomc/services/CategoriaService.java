9a938546d15343f10192550182462b802f86b005
Weverton Avelino <wtonja@gmail.com>
2019-10-07 21:05:42 -0300

    validacao sintatica com Bean validation

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceotions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceotions.ObjectNotFoundException;
@@ -59,4 +60,7 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto ) {
+		return new Categoria(objDto.getId(), objDto.getNome());		
+	}
 }
